d99da895e91a38271369b322bf5a08ed74a70ff0
EC2 Default User <mkspider42@gmail.com>
2022-08-25 05:41:41 +0000

    タグ検索機能アソシエーションまで

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,8 @@ class Movie < ApplicationRecord
   has_many :reviews,dependent: :destroy
   has_many :comments,dependent: :destroy
   belongs_to :genre
+  has_many :tagmaps, dependent: :destroy
+  has_many :tags, through: :tagmaps
 
 def self.search(search_word)
     where('filmname LIKE ? OR introduction LIKE ?', "%#{keyword}%", "%#{keyword}%")
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_24_160346) do
+ActiveRecord::Schema.define(version: 2022_08_25_052159) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -85,6 +85,22 @@ ActiveRecord::Schema.define(version: 2022_08_24_160346) do
     t.string "title"
   end
 
+  create_table "tagmaps", force: :cascade do |t|
+    t.integer "movie_id", null: false
+    t.integer "tag_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["movie_id"], name: "index_tagmaps_on_movie_id"
+    t.index ["tag_id"], name: "index_tagmaps_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "tag_name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tag_name"], name: "index_tags_on_tag_name", unique: true
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -103,4 +119,6 @@ ActiveRecord::Schema.define(version: 2022_08_24_160346) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "tagmaps", "movies"
+  add_foreign_key "tagmaps", "tags"
 end
